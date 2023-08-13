89be0ccf696e34511c6a05db4296c198682d5fba
Andri Wandres <andrilucawandres@gmail.com>
2023-08-13 18:36:51 +0200

    ci: use correct artifacts location path

--- build/Constants/SyrinjectPaths.cs
+++ build/Constants/SyrinjectPaths.cs
@@ -2,10 +2,11 @@
 
 internal static class SyrinjectPaths
 {
-    public const string ArtifactsDirectoryPath = "artifacts";
     public const string BuildFolderName = "build";
+    public const string ArtifactsDirectoryPath = $"{BuildFolderName}/artifacts";
 
-    public const string SolutionFilePath = "src/Syrinject.sln";
+    private const string SourceFolderName = "src";
+    public const string SolutionFilePath = $"{SourceFolderName}/Syrinject.sln";
 
     public const string NuGetPackageSource = "https://api.nuget.org/v3/index.json";
 }
